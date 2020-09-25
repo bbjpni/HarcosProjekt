cd4d2e445af872e4f7c4532b074f1d1a3d302f17
Zsombor <zsomi72@gmail.com>
2020-09-25 10:07:16 +0200

    Add project files.

---
+++ HarcosProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HarcosProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
