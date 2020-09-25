53106ee04b1f3ab1223bfe13762ffc56e5f93866
Zsombor <zsomi72@gmail.com>
2020-09-25 13:01:53 +0200

    Hibás bekeres

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -17,6 +17,23 @@ namespace HarcosProjekt
             }
             sr.Close();
         }
+        static void userCreat()
+        {
+            bool exit = false;
+            Console.WriteLine("STATUSZ SABLONOK:\n\t[1] HP:18/18 - DMG: 4\n\t[2] HP:15/15 - DMG: 5\n\t[3] HP:11/11 - DMG: 6");
+            do
+            {
+                Console.Write("\nAdja meg a harcos nevét:");
+                string nev = Console.ReadLine();
+                Console.Write("\nAdja meg a harcos sablonját:");
+                string beSzam = Console.ReadLine();
+                exit = nev != "";
+                int szam = 0;
+                exit = exit && Int32.TryParse(beSzam, out szam);
+                exit = exit && (szam > 0 || szam < 4);
+                
+            } while (!exit);
+        }
         static void Main(string[] args)
         {
             tagok = new List<Harcos>() { new Harcos("ZOLTÁÁN", 2), new Harcos("OSzablcs", 1), new Harcos("Zsombor", 3) };
@@ -25,6 +42,7 @@ namespace HarcosProjekt
             {
                 Console.WriteLine(tagok[i]);
             }
+            userCreat();
         }
     }
 }
