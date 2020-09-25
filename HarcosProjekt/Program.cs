d8c2e25694f35de9d7ce97d34eb819163c416d50
Zsombor <zsomi72@gmail.com>
2020-09-25 12:30:01 +0200

    Harcos tipusu lista 3 harcossal

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HarcosProjekt
 {
     class Program
     {
+        static List<Harcos> tagok;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tagok = new List<Harcos>() { new Harcos("ZOLTÁÁN", 2), new Harcos("OSzablcs", 1), new Harcos("Zsombor", 3) };
         }
     }
 }
