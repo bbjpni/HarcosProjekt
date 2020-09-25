e7774096057a02f2378de2da846e74aa701854df
Zsombor <zsomi72@gmail.com>
2020-09-25 10:32:10 +0200

    Létrehoz: Harcos.cs [Sebzes, Szintlepeshez, MaxEletero]

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -22,9 +22,9 @@ namespace HarcosProjekt
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
         public int Eletero { get => eletero; set => eletero = value; }
-        //public int Sebzes() { get =>  }
-        //public int Szintlepeshez() { get =>  }
-        //public int MaxEletero() { get => }
+        public int Sebzes { get => alapSebzes + szint; }
+        public int Szintlepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => alapEletero + szint * 3; }
         //public void Megkuzd(Harcos masikHarcos) {}
         //public void Gyogyul() {}
         //public override string ToString()
