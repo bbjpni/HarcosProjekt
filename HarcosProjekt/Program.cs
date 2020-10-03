5528920ad524ad431c28ea880a36969607c052bb
Zsombor <brozsek.zsombor@petrik.hu>
2020-10-03 18:32:42 +0200

    felhasznalo ltrehoz harcost, kiirat

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -17,22 +17,24 @@ namespace HarcosProjekt
             }
             sr.Close();
         }
-        static void userCreat()
+        static Harcos userCreat()
         {
+            int szam = 0;
+            string nev = "";
             bool exit = false;
-            Console.WriteLine("STATUSZ SABLONOK:\n\t[1] HP:18/18 - DMG: 4\n\t[2] HP:15/15 - DMG: 5\n\t[3] HP:11/11 - DMG: 6");
+            Console.WriteLine("\nSTATUSZ SABLONOK:\n\t[1] HP:18/18 - DMG: 4\n\t[2] HP:15/15 - DMG: 5\n\t[3] HP:11/11 - DMG: 6");
             do
             {
-                Console.Write("\nAdja meg a harcos nevét:");
-                string nev = Console.ReadLine();
-                Console.Write("\nAdja meg a harcos sablonját:");
+                Console.Write("\nAdja meg a harcos nevét: ");
+                nev = Console.ReadLine();
+                Console.Write("\nAdja meg a harcos sablonját: ");
                 string beSzam = Console.ReadLine();
                 exit = nev != "";
-                int szam = 0;
                 exit = exit && Int32.TryParse(beSzam, out szam);
-                exit = exit && (szam > 0 || szam < 4);
+                exit = exit && (szam > 0 && szam < 4);
                 
             } while (!exit);
+            return new Harcos(nev, szam);
         }
         static void Main(string[] args)
         {
@@ -42,7 +44,8 @@ namespace HarcosProjekt
             {
                 Console.WriteLine(tagok[i]);
             }
-            userCreat();
+            tagok.Add(userCreat());
+            Console.WriteLine("\n"+tagok[tagok.Count-1]);
         }
     }
 }
